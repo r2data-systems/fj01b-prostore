4743ceea788a2813b1cc99217619c81672246945
r2data-systems <chris@r2data-systems.com>
2025-08-26 18:19:08 -0400

    12; Select theme from w/ dropdown menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
 	return (
@@ -18,13 +19,14 @@ const Header = () => {
 					</Link>
 				</div>
 				<div className="space-x-2">
+					<ModeToggle />
 					<Button asChild variant='ghost' >
 						<Link href='/cart'>
 							<ShoppingCart/>Cart
 						</Link>
 					</Button>
-					<Button asChild variant='ghost' >
-						<Link href='/signin'>
+					<Button asChild >
+						<Link href='/sign-in'>
 							<UserIcon/>Sign In
 						</Link>
 					</Button>
