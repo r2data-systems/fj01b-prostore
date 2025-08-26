e5af270293c32b6d1cf9d3d753923775bb974904
r2data-systems <chris@r2data-systems.com>
2025-08-26 11:10:50 -0400

    11ii; Footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/shared/header";
 
 export default function RootLayout({
@@ -11,6 +12,7 @@ export default function RootLayout({
 			<main className="flex-1 wrapper">
 				{children}
 			</main>
+			<Footer/>
 		</div>
 	);
 }
