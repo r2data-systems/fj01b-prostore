2313b39c515d32b8e693c123d7c9798d65a9937b
r2data-systems <chris@r2data-systems.com>
2025-08-27 08:42:13 -0400

    13iii; delay removed, code left in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,12 @@
 import { Button } from "@/components/ui/button";
 
-const delayTest1 = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const metadata = {
 	title: 'Home'
 };
 
 // make fn async for delayTest1 to work
-const HomePage = async () => {
-	//await delayTest1(3000);
-	await new Promise(resolve => setTimeout(resolve, 1000));
+const HomePage =  () => {
+	//await new Promise(resolve => setTimeout(resolve, 1000));
 
 	return ( <Button>Prostore</Button> );
 }
