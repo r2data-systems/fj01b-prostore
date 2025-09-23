562a8d5d7004903cdeca407ac33beda6d5b10883
r2data-systems <chris@r2data-systems.com>
2025-09-23 19:48:49 -0400

    35ii; SignInPage added Card elements w/ logo

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,24 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: 'Sign In'
+};
+
 const SignInPage = () => {
 	return (
-		<>Sign In Page</>
+		<div className="w-full max-w-md mx-auto">
+			<Card>
+				<CardHeader className="space-y-4">
+					<Link href='/' className="flex-center">
+						<Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
+					</Link>
+				</CardHeader>
+			</Card>
+		</div>
 	);
 }
  
