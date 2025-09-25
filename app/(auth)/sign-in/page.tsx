503201565bf9418226c061f4b1da1284fff0f557
r2data-systems <chris@r2data-systems.com>
2025-09-25 10:11:05 -0400

    36; Credentials(email/password) Sign In form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
 	title: 'Sign In'
@@ -10,21 +11,29 @@ export const metadata: Metadata = {
 
 const SignInPage = () => {
 	return (
-		<div className="w-full max-w-md mx-auto">
-			<Card>
-				<CardHeader className="space-y-4">
-					<Link href='/' className="flex-center">
-						<Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
-					</Link>
-					<CardTitle className="text-center">Sign In</CardTitle>
-					<CardDescription className="text-center">Sign in to your account</CardDescription>
-				</CardHeader>
-				<CardContent className="space-y-4">
-					{/* Form Here */}
-				</CardContent>
-			</Card>
-		</div>
-	);
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
  
 export default SignInPage;
