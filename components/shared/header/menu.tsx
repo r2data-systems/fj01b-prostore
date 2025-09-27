86c6bf4b4efef8ea4f8c1efb0e2f779bcde7b3fc
r2data-systems <chris@r2data-systems.com>
2025-09-27 19:06:19 -0400

    39i; user-button skeleton implemented in menu

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -3,6 +3,7 @@ import ModeToggle from "./mode-toggle";
 import Link from "next/link";
 import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
 	return (
@@ -15,12 +16,7 @@ const Menu = () => {
             Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon />
-            Sign In
-          </Link>
-        </Button>
+				<UserButton/>
       </nav>
       <nav className="md:hidden">
         <Sheet>
