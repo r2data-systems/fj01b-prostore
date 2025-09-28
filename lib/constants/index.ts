bce510b19c738ae8b3debe37a2e980413cf76e7b
r2data-systems <chris@r2data-systems.com>
2025-09-28 18:37:44 -0400

    39iii; if signed in; initial user-button

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,6 +3,6 @@ export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'A modern e-commerce
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 export const signInDefaultValues = {
-	email: 'chris@example.com',
+	email: 'admin@example.com',
 	password: ''
 }
