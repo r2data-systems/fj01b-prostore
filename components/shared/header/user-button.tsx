24845966a6b3bf3dac386d3deba0a9452d9f2327
r2data-systems <chris@r2data-systems.com>
2025-09-29 18:43:48 -0400

    39iv; user-button details name, email and signout button

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -22,18 +22,38 @@ const UserButton = async () => {
 	// return; if logged in
 	const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? '?';
   return (
-		<div className="flex gap-2 items-center">
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<div className="flex items-center">
-						<Button variant='ghost' className="relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-200">
-							{firstInitial}
-						</Button>
-					</div>
-				</DropdownMenuTrigger>
-			</DropdownMenu>
-		</div>
-	);
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
+            <Button
+              variant="ghost"
+              className="relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-200"
+            >
+              {firstInitial}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <div className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </div>
+              <div className="text-sm text-muted-foreground leading-none">
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+					<DropdownMenuItem className="p-0 mb-1">
+						<form action={signOutUser} className="w-full">
+							<Button className="w-full py-4 px-2 h-4 justify-start" variant='ghost'>Sign Out</Button>
+						</form>
+					</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
  
 export default UserButton;
