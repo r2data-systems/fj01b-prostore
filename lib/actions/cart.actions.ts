fb6cf63458ac44c86c5d91e6a31022e4d0a81dca
r2data-systems <chris@r2data-systems.com>
2025-10-28 23:53:06 +0000

    49; cart.actions.ts add fn getMyCart, addItemToCart & calcPrice plus utils.ts fn round2

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,10 +2,28 @@
 
 import { cookies } from "next/headers";
 import { CartItem } from "@/types";
-import { convert2PlainObject, formatErrors } from "../utils";
+import { convert2PlainObject, formatErrors, round2 } from "../utils";
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
-import { cartItemSchema } from "../validators";
+import { cartItemSchema, insertCartSchema } from "../validators";
+import { revalidatePath } from "next/cache";
+
+// Calculate Cart Prices
+const calcPrice = (items: CartItem[]) => {
+	const itemsPrice = round2(
+		items.reduce((acc, item) => (acc + Number(item.price) * item.qty), 0)
+	),
+	shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+	taxPrice = round2(itemsPrice * 0.15),
+	totalPrice = round2(itemsPrice + shippingPrice + taxPrice)
+
+	return {
+		itemsPrice: itemsPrice.toFixed(2),
+		shippingPrice: shippingPrice.toFixed(2),
+		taxPrice: taxPrice.toFixed(2),
+		totalPrice: totalPrice.toFixed(2),
+	}
+};
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -31,17 +49,41 @@ export async function addItemToCart(data: CartItem) {
 			where: {id: item.productID}
 		});
 
+		if (!product) {throw new Error('Product NOT found')};
+
+		if (!cart) {
+			// Create new cart object
+			const newCart = insertCartSchema.parse({
+				userID: userID,
+				items: [item],
+				sessionCartID: sessionCartID,
+				...calcPrice([item])
+			});
+
+			//console.log(newCart);
+
+			// Add to db
+			await prisma.cart.create({
+				data: newCart
+			})
+
+			// Revalidate product page
+			revalidatePath(`/product/${product.slug}`);
+
+			return {
+				success: true,
+				message: "Item added to Cart",
+			};
+		}
+
     // Test
-    console.log({
-      "Session Cart ID": sessionCartID,
-      "User ID": userID,
-			"Item Requested": item,
-			"Product Found": product
-    });
-    return {
-      success: true,
-      message: "Item added to Cart",
-    };
+    //console.log({
+    //  "Session Cart ID": sessionCartID,
+    //  "User ID": userID,
+		//	"Item Requested": item,
+		//	"Product Found": product,
+    //});
+
   } catch (error) {
     return {
       success: false,
--- lib/utils.ts
+++ lib/utils.ts
@@ -46,3 +46,14 @@ export function formatErrors(error: any) {
 
 	}
 };
+
+// Round number to 2 decimal places
+export function round2(value: number | string) {
+	if (typeof value === 'number') {
+		return Math.round((value + Number.EPSILON) * 100) / 100;
+	} else if (typeof value === 'string') {
+		return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+	} else {
+		throw new Error('Value is NOT a number or a string');
+	};
+};
