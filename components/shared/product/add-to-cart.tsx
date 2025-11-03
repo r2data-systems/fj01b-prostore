d7bfd1a93f3b995425316e7d63b687428fb31885
r2data-systems <chris@r2data-systems.com>
2025-11-03 22:28:02 +0000

    52; add to cart & remove from cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,13 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -38,11 +38,36 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
-    <Button className="w-full" type="button" onClick={handleAdd2Cart}>
-      <Plus />Add to Cart
-    </Button>
-  );
+	// Handler for removeItemFromCart
+	const handleRemoveItemFromCart = async () => {
+		const res = await removeItemFromCart(item.productID);
+
+		toast({
+			variant: res.success ? "default" : "destructive",
+			description: res.message,
+		});
+
+		return;
+	};
+
+	// Check if item is in cart
+	const existItem = cart && cart?.items.find((x) => (x.productID === item.productID));
+
+  return ( existItem ? (
+		<div>
+			<Button type="button" variant="outline" onClick={handleRemoveItemFromCart}>
+				<Minus className="h-4 w-4"/>
+			</Button>
+			<span className="px-2">{existItem.qty}</span>
+			<Button type="button" variant="outline" onClick={handleAdd2Cart}>
+				<Plus className="h-4 w-4"/>
+			</Button>
+		</div>
+	) : (
+		<Button className="w-full" type="button" onClick={handleAdd2Cart}>
+			<Plus />Add to Cart
+		</Button>
+	));
 };
 
 export default AddToCart;
