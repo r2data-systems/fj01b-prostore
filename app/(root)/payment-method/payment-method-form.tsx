7eacd465388a5c550d8b464208e11d8cdd656db7
r2data-systems <chris@r2data-systems.com>
2025-12-03 23:26:18 +0000

    66; Payment Method page and payment-method-form

---
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { useTransition } from "react";
+import { paymentMethodSchema } from "@/lib/validators";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+
+const PaymentMethodForm = ({preferredPaymentMethod}: {preferredPaymentMethod: string | null}) => {
+	console.log(preferredPaymentMethod);
+	const router = useRouter();
+	const {toast} = useToast();
+
+	const form = useForm<z.infer<typeof paymentMethodSchema>>({
+		resolver: zodResolver(paymentMethodSchema),
+		defaultValues: {
+			type: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD
+		}
+	});
+
+	const [isPending, startTransistion] = useTransition();
+
+	return ( 
+		<>
+			<CheckoutSteps current={2}/>
+		</>
+	);
+}
+ 
+export default PaymentMethodForm;
