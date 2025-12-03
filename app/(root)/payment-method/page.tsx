17f85291bfaeaedc20db93daade9d917de05c469
r2data-systems <chris@r2data-systems.com>
2025-12-03 23:26:18 +0000

    66; Payment Method page and payment-method-form

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserByID } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata: Metadata = {
+	title: 'Select Payment Method',
+};
+
+const PaymentMethodPage = async () => {
+	const session = await auth();
+	const userID = session?.user?.id;
+
+	if (!userID) { throw new Error('User NOT found!')};
+
+	const user = await getUserByID(userID);
+	
+	return ( <>
+		Payment Method
+		<PaymentMethodForm preferredPaymentMethod={user.paymentMethod}/>
+	</> );
+}
+ 
+export default PaymentMethodPage;
