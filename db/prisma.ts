0eb3c65c4715e50caf652ba4d1b8f27449b68ad4
r2data-systems <chris@r2data-systems.com>
2025-12-31 15:16:29 +0000

    74; Format Utility Functions - prisma.ts and utils.ts

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,9 +1,9 @@
 // prisma-V6.ts
 // or relative, like '../../generated/prisma'
-//import { PrismaClient } from '@/lib/generated/prisma'; 
-import { PrismaClient } from '@prisma/client'; 
-import { PrismaNeon } from '@prisma/adapter-neon';
- 
+//import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
@@ -16,6 +16,65 @@ export const prisma = new PrismaClient({ adapter }).$extends({
       rating: {
         compute(product) {
           return product.rating.toString();
+        },
+      },
+    },
+    cart: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(cart) {
+          return cart.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(cart) {
+          return cart.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(cart) {
+          return cart.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(cart) {
+          return cart.totalPrice.toString();
+        },
+      },
+    },
+    order: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(cart) {
+          return cart.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(cart) {
+          return cart.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(cart) {
+          return cart.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(cart) {
+          return cart.totalPrice.toString();
+        },
+      },
+    },
+    orderItem: {
+      price: {
+        compute(cart) {
+          return cart.price.toString();
         },
       },
     },
--- lib/utils.ts
+++ lib/utils.ts
@@ -73,3 +73,59 @@ export function formatCurrency(amount: number|string|null) {
 		return 'NaN';
 	}
 };
+
+// Shorten UUID
+export function shortenId(id: string) {
+	return `..${id.substring(id.length - 6)}`;
+}
+
+//console.log(shortenId('d57dfd2d-3ae6-4276-92c4-19a0fc886475'));
+
+// Format date and times
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: 'short', // abbreviated month name (e.g., 'Oct')
+    year: 'numeric', // abbreviated month name (e.g., 'Oct')
+    day: 'numeric', // numeric day of the month (e.g., '25')
+    hour: 'numeric', // numeric hour (e.g., '8')
+    minute: 'numeric', // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short', // abbreviated weekday name (e.g., 'Mon')
+    month: 'short', // abbreviated month name (e.g., 'Oct')
+    year: 'numeric', // numeric year (e.g., '2023')
+    day: 'numeric', // numeric day of the month (e.g., '25')
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric', // numeric hour (e.g., '8')
+    minute: 'numeric', // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateTimeOptions
+  );
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateOptions
+  );
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    timeOptions
+  );
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+};
+
+const testDate = new Date('2023-10-25T08:30:00Z');
+
+const formatted = formatDateTime(testDate);
+
+// Log the results
+console.log('Full DateTime:', formatted.dateTime);
+console.log('Date Only', formatted.dateOnly);
+console.log('Time Only', formatted.timeOnly);
