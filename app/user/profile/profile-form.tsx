1e55a66653b3f13610aa383f12aa45f67ed516cd
r2data-systems <chris@r2data-systems.com>
2026-02-06 23:19:00 +0000

    92; Profile Form Submission and auth changes

--- app/user/profile/profile-form.tsx
+++ app/user/profile/profile-form.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
+import { updateProfile } from "@/lib/actions/user.actions";
 import { updateProfileSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSession } from "next-auth/react";
@@ -23,8 +24,33 @@ const ProfileForm = () => {
 
 	const { toast } = useToast();
 	
-	const onSubmit = () => {
-		return;
+	const onSubmit = async (values: z.infer<typeof updateProfileSchema>) => {
+		const res = await updateProfile(values);
+		
+		if (!res.success) {
+			return toast ({
+				variant: "destructive",
+				description: res.message
+			});
+		}
+
+		// this creates the object but doesn't do anything with it
+		const newSession = {
+      ...session,
+      user: {
+        ...session?.user,
+        name: values.name,
+      },
+    };
+
+		await update(newSession);
+
+		toast ({
+			description: res.message
+		})
+
+		// However this doesn't update the token
+		// go to auth.ts > jwt
 	}
 
 	return (
