3f00d44dbbc20fa5f0e13aecf53c9be9fb60118d
r2data-systems <chris@r2data-systems.com>
2026-02-19 17:10:54 +0000

    99; Admin Orders Page & Action

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,66 @@
+import Pagination from "@/components/shared/pagination";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { requireAdmin } from "@/lib/auth-guard";
+import { formatCurrency, formatDateTime, shortenId } from "@/lib/utils";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: 'Admin Orders',
+};
+
+const AdminOrdersPage = async (props: {
+	searchParams: Promise<{page: string}>
+}) => {
+	await requireAdmin();
+
+	const {page = '1'} = await props.searchParams;
+
+	const orders = await getAllOrders({
+		page: Number(page),
+	});
+	
+	return (
+		<div className="space-y-2">
+        <h2 className="h2-bold">Orders Page</h2>
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>DATE</TableHead>
+                <TableHead>TOTAL</TableHead>
+                <TableHead>PAID</TableHead>
+                <TableHead>DELIVERED</TableHead>
+                <TableHead>ACTIONS</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.data.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{shortenId(order.id)}</TableCell>
+                  <TableCell>{formatDateTime(order.createdAt).dateTime} </TableCell>
+                  <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                  <TableCell>{order.isPaid && order.paidAt ? formatDateTime(order.paidAt).dateTime : "Not Paid"} </TableCell>
+                  <TableCell>{order.isDelivered && order.deliveredAt ? formatDateTime(order.deliveredAt).dateTime : "Not Delivered"} </TableCell>
+                  <TableCell>
+										<Button asChild variant='outline' size='sm'>
+											<Link href={`/order/${order.id}`}>Details</Link>
+										</Button>
+										{/*Delete Button*/}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+					{ orders.totalPages > 1 && (
+						<Pagination page={ Number(page) || 2 } totalPages={orders?.totalPages}></Pagination>
+					)}
+        </div>
+      </div>
+	);
+}
+ 
+export default AdminOrdersPage;
