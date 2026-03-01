e242c58790d6fb8b391880348c43c9443e327147
r2data-systems <chris@r2data-systems.com>
2026-03-01 23:57:25 +0000

    103 & 104; fn getAllProducts for Admin Page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,28 @@
+import { getAllProducts } from "@/lib/actions/product.actions";
+
+const AdminProductsPage = async (props: {
+		searchParams: Promise<{page: string; query: string; category: string;}>
+	}) => {
+	const searchParams = await props.searchParams;
+	const page = Number(searchParams.page) || 1;
+	const searchText = searchParams.query || '';
+	const category = searchParams.category || '';
+
+	const products = await getAllProducts({
+		query: searchText,
+		page,
+		category,
+	})
+	
+	console.log(products);
+	
+	return (
+		<div className="space-y-2">
+			<div className="flex-between">
+				<h1 className="h2-bold">Products</h1>
+			</div>
+		</div>
+	);
+}
+ 
+export default AdminProductsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,7 +3,7 @@
 //import { PrismaClient } from '@/lib/generated/prisma';
 import {prisma} from '@/db/prisma';
 import { convert2PlainObject } from '../utils';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 
 export async function getLatestProducts() {
 	// Only needs to be instantiated in prisma.ts
@@ -21,5 +21,29 @@ export async function getProductBySlug(slug: string) {
 	return prisma.product.findFirst({
 		where: {slug: slug},
 	});
+}
+
+// Get all products
+export async function getAllProducts({
+	query,
+	limit = PAGE_SIZE,
+	page,
+	category
+}: {
+	query: string;
+	limit?: number;
+	page: number;
+	category?: string;
+}) {
+	const data = await prisma.product.findMany({
+		skip: (page - 1) * limit,
+		take: limit,
+	});
 	
+	const dataCount = await prisma.product.count();
+
+	return({
+		data,
+		totalPages: Math.ceil(dataCount / limit),
+	})
 }
