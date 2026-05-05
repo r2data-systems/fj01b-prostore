26690467df525676ca17eae527821dd0a40b0265
r2data-systems <chris@r2data-systems.com>
2026-05-05 22:57:38 +0100

    124; Admin User Search

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,6 +10,7 @@ import { formatError } from "../utils";
 import { z } from 'zod';
 import { PAGE_SIZE } from '../constants';
 import { revalidatePath } from 'next/cache';
+import { Prisma } from '@prisma/client';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -175,11 +176,21 @@ export async function updateProfile(user: {name: string; email: string;}) {
 export async function getAllUsers({
 	limit = PAGE_SIZE,
 	page,
+	query,
 }: {
 	limit?: number;
 	page: number;
+	query: string;
 }) {
+	const queryFilter: Prisma.UserWhereInput = query && query !== 'all' ? {
+		name: {
+			contains: query,
+			mode: 'insensitive'
+		} as Prisma.StringFilter
+	} : {};
+
 	const data = await prisma.user.findMany({
+		where: {...queryFilter},
 		orderBy: {createdAt: 'desc'},
 		take: limit,
 		skip: (page - 1) * limit,
