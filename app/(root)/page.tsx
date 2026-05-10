77975e9e7bf7d6b5972609c2a2337e0ebcbb60d9
r2data-systems <chris@r2data-systems.com>
2026-05-10 23:07:47 +0100

    127; Featured Products Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 //import sampleData from "@/db/sample-data";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 import ProductList from "@/components/shared/product/product-list";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 export const metadata = {
 	title: 'Home'
@@ -11,9 +12,11 @@ const HomePage =  async () => {
 	//await new Promise(resolve => setTimeout(resolve, 1000));
 	console.log('sampleData.products');
 	const latestProducts = await getLatestProducts();
+	const featuredProducts = await getFeaturedProducts();
 
 	return ( 
 		<>
+			{ featuredProducts.length > 0 && <ProductCarousel data={featuredProducts}/>}
 			<ProductList data={latestProducts} title="Newest Arrivals" limit={4}/>
 		</>
 	);
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -118,6 +118,7 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
 		return {success: false, message: formatError(error)};
 	}
 }
+
 // Get ALL categories
 export async function getAllCategories() {
 	const data = await prisma.product.groupBy({
@@ -126,4 +127,15 @@ export async function getAllCategories() {
 	});
 
 	return data;
+}
+
+// Get featured products
+export async function getFeaturedProducts() {
+	const data = await prisma.product.findMany({
+		where: {isFeatured: true},
+		orderBy: {createdAt: 'desc'},
+		take: 4,
+	})
+	
+	return convert2PlainObject(data);
 }
