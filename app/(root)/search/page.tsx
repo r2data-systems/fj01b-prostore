03e6c362ba8d689ea956658f8e97f9551dada687
r2data-systems <chris@r2data-systems.com>
2026-05-22 14:43:03 +0100

    131; getFilterURL fn

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,17 +1,17 @@
-import ProductCard from "@/components/shared/product/product-card";
-import { getAllProducts } from "@/lib/actions/product.actions";
+import ProductCard from '@/components/shared/product/product-card';
+import { getAllProducts } from '@/lib/actions/product.actions';
 
 const SearchPage = async (props: {
-	searchParams: Promise<{
-		q?: string;
-		category?: string;
-		price?: string;
-		rating?: string;
-		sort?: string;
-		page?: string;
-	}>;
+  searchParams: Promise<{
+    q?: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+    sort?: string;
+    page?: string;
+  }>;
 }) => {
-	const {
+  const {
     q = 'all',
     category = 'all',
     price = 'all',
@@ -20,32 +20,56 @@ const SearchPage = async (props: {
     page = '1',
   } = await props.searchParams;
 
-	console.log( q, category, price, rating, sort, page );
+  console.log(q, category, price, rating, sort, page);
 
-	const products = await getAllProducts({
+  const getFilterURL = ({
+    c,
+    p,
+    r,
+    s,
+    pg,
+  }: {
+    c?: string;
+		p?: string;
+    r?: string;
+    s?: string;
+    pg?: string;
+  }) => {
+		const params = {q, category, price, rating, sort, page};
+
+		if (c) params.category = c;
+		if (p) params.price = p;
+		if (r) params.rating = r;
+		if (s) params.sort = s;
+		if (pg) params.page = pg;
+
+		return `/search?${new URLSearchParams(params).toString()}`
+	};
+
+  const products = await getAllProducts({
     query: q,
     category,
     price,
     rating,
     sort,
-    page: Number(page)
+    page: Number(page),
   });
 
-	return (
-		<div className="grid md:grid-cols-5 md:gap-5">
-			<div className="filter-links">
-				{/*FILTER_LINKS*/}
+  return (
+    <div className="grid md:grid-cols-5 md:gap-5">
+      <div className="filter-links">{/*FILTER_LINKS*/}
+				URL: {getFilterURL({c: 'Mens Sweat Shirts'})}
 			</div>
-			<div className="md:col-span-4 space-y-4">
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-					{ products.data.length === 0 && <div>No Products Found</div> }
-					{ products.data.map((product) => (
-						<ProductCard key={product.id} product={product}/>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+      <div className="md:col-span-4 space-y-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {products.data.length === 0 && <div>No Products Found</div>}
+          {products.data.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
