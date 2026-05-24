d0ba68a3c4fdb2f730329fd59684d9f2255a2523
r2data-systems <chris@r2data-systems.com>
2026-05-24 14:11:47 +0100

    132; Add Category and Price Links which enable filtering

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,5 +1,14 @@
 import ProductCard from '@/components/shared/product/product-card';
-import { getAllProducts } from '@/lib/actions/product.actions';
+import { getAllProducts, getAllCategories } from '@/lib/actions/product.actions';
+import Link from 'next/link';
+
+const prices = [
+	{name: '$1 to $50', value: '1-50'},
+	{name: '$51 to $100', value: '51-100'},
+	{name: '$101 to $200', value: '101-200'},
+	{name: '$201 to $500', value: '201-500'},
+	{name: '$501 to $999', value: '501-999'},
+]
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -55,10 +64,41 @@ const SearchPage = async (props: {
     page: Number(page),
   });
 
+	const categories = await getAllCategories();
+
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/*FILTER_LINKS*/}
-				URL: {getFilterURL({c: 'Mens Sweat Shirts'})}
+      <div className="filter-links">
+				{/*URL: {getFilterURL({c: 'Mens Sweat Shirts'})}*/}
+				{/*Category Links*/}
+				<div className="text-xl mb-2 mt-3">Category</div>
+				<div>
+					<ul className="space-y-1">
+						<li>
+							<Link className={`${(category === 'all' || category === '') && 'font-bold'}`} href={getFilterURL({c: 'all'})}>All</Link>
+						</li>
+						{ categories.map((c) => (
+							<li key={c.category}>
+								<Link className={`${(category === c.category) && 'font-bold'}`} href={getFilterURL({c: c.category})}>{c.category}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+				{/*Price Links*/}
+				<div className="text-xl mb-2 mt-8">Prices</div>
+				<div>
+					<ul className="space-y-1">
+						<li>
+							<Link className={`${(price === 'all') && 'font-bold'}`} href={getFilterURL({p: 'all'})}>All</Link>
+						</li>
+						{ prices.map((p) => (
+							<li key={p.value}>
+								<Link className={`${(price === p.value) && 'font-bold'}`} href={getFilterURL({p: p.value})}>{p.name}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			
 			</div>
       <div className="md:col-span-4 space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
