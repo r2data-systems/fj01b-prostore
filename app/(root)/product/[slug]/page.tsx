029aa93e07105c845eaa0ee0742eda43d6ab2207
r2data-systems <chris@r2data-systems.com>
2026-06-29 00:12:22 +0100

    143; Display Reviews incl rating stars

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,7 @@ import AddToCart from "@/components/shared/product/add-to-cart"
 import { getMyCart } from "@/lib/actions/cart.actions";
 import { auth } from "@/auth";
 import ReviewList from "./review-list";
+import Rating from "@/components/shared/product/rating";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -39,9 +40,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className="h3-bold">{product.name}</h1>
-              <p>
-                {product.rating} of {product.numReviews} Reviews
-              </p>
+							<Rating value={Number(product.rating)} />
+              <p> {product.numReviews} Reviews </p>
               <div className="flex flex-col sm:items-center sm:flex-row gap-3">
                 <ProductPrice
                   value={Number(product.price)}
--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -2,8 +2,13 @@
 
 import { Review } from "@/types";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { User, Calendar } from "lucide-react";
 import ReviewForm from "./review-form";
+import { getReviews } from "@/lib/actions/review.actions";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatDateTime } from "@/lib/utils";
+import Rating from "@/components/shared/product/rating";
 
 const ReviewList = ({userID, productID, productSlug}: {
 	userID: string;
@@ -14,29 +19,73 @@ const ReviewList = ({userID, productID, productSlug}: {
 
 	const [reviews, setReviews] = useState<Review[]>([]);
 
+	useEffect(() => {
+		const loadReviews = async () => {
+			const res = await getReviews({productID});
+			setReviews(res.data);
+		};
+
+		loadReviews();
+	}, [productID]);
+
 	const reload = () => {
 		console.log('Review Submitted');
 	};
 
 	return (
-		<div className="space-y-4">
-			{reviews.length === 0 && <div>No Reviews Available</div>}
-			{userID ? (
-					<>
-						{/*REVIEW LINK HERE*/}
-						<ReviewForm userID={userID} productID={productID} onReviewSubmitted={reload}/>
-					</>
-				) : (
-					<div>
-						Please
-						<Link className="text-blue-700 px-2" href={`/sign-in?callbackUrl=/product/${productSlug}`}>Sign In</Link>
-						to write a review
-					</div>
-				)
-			}
-			<div className="flex flex-col gap-3">{/* REVIEWS HERE */}</div>
-		</div>
-	);
+    <div className="space-y-4">
+      {reviews.length === 0 && <div>No Reviews Available</div>}
+      {userID ? (
+        <>
+          {/*REVIEW LINK HERE*/}
+          <ReviewForm
+            userID={userID}
+            productID={productID}
+            onReviewSubmitted={reload}
+          />
+        </>
+      ) : (
+        <div>
+          Please
+          <Link
+            className="text-blue-700 px-2"
+            href={`/sign-in?callbackUrl=/product/${productSlug}`}
+          >
+            Sign In
+          </Link>
+          to write a review
+        </div>
+      )}
+      <div className="flex flex-col gap-3">
+        {/* REVIEWS HERE */}
+        {reviews.map((review) => (
+          <Card key={review.id}>
+            <CardHeader>
+              <div className="flex-between">
+                <CardTitle>{review.title}</CardTitle>
+              </div>
+              <CardDescription>{review.description}</CardDescription>
+            </CardHeader>
+						<CardContent>
+							<div className="flex space-x-4 text-sm text-muted-foreground">
+								{/*RATINGS*/}
+								<Rating value={review.rating} />
+								<div className="flex items-center">
+									{/*user icon*/}
+									<User className="mr-1 h-3 w-3"/>
+									{review.user ? review.user.name : 'User' }
+								</div>
+								<div className="flex items-center">
+									<Calendar className="mr-1 h-3 w-3"/>
+									{formatDateTime(review.createdAt).dateTime}
+								</div>
+							</div>
+						</CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
  
 export default ReviewList;
