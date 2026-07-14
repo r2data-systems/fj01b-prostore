7e52f67f534dd2fb9494679666c187c650f67244
r2data-systems <chris@r2data-systems.com>
2026-07-14 21:55:28 +0100

    VRCL06; .env alternative, tidy up main

--- db/upload-thing-sdk-v7/upload-V2.ts
+++ db/upload-thing-sdk-v7/upload-V2.ts
@@ -1,52 +1,28 @@
 import fs from "fs";
-
-/* Code to use local .env*/ 
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import { UTApi } from "uploadthing/server";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//dotenv.config({
-//  path: path.join(__dirname, ".env"),
-//  override: true,
-//});
-
 const env = dotenv.config({
-  path: path.join(__dirname, ".env"),
+	path: path.join(__dirname, ".env"),
+	  override: true,
 }).parsed;
 
 const token = env?.UPLOADTHING_TOKEN;
 const secret = env?.UPLOADTHING_SECRET;
 
-//import path from "path";
-import { UTApi } from "uploadthing/server";
-//import 'dotenv/config';
-
-//import dotenv from "dotenv";
-
-//const result = dotenv.config();
-//console.dir(result);
-
-//import path from "path";
-
 console.log("cwd =", process.cwd());
 console.log("env file =", path.resolve(process.cwd(), ".env"));
 console.log("UPLOADTHING_TOKEN =", !!process.env.UPLOADTHING_TOKEN);
-
-//console.log("UPLOADTHING_APP_ID",process.env.UPLOADTHING_APP_ID);
-
-// Initialize UploadThing API
-//const utapi = new UTApi({
-//	token: process.env.UPLOADTHING_TOKEN
-//});
 
 const utapi = new UTApi({
 	token: token
 });
 
-//console.log(process.env.UPLOADTHING_SECRET?.slice(0, 12));
 console.log('secret;',secret?.slice(0, 12));
 
 // Read JSON file
